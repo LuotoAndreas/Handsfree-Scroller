7ee4e42f2c175b8cbad3dfa4b76db10142806d8c
luotajyz <luotajyz@jyu.fi>
2025-06-13 19:50:16 +0300

    documentation and optimized hand hestures

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import cv2
 import mediapipe as mp
 import pyautogui
 
+# Initializes the MediaPipe Hands solution for hand tracking
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=1)
 mp_draw = mp.solutions.drawing_utils
 
+# Opens the webcam video capture
 cap = cv2.VideoCapture(0)
+
+# Fuctio that returns true if finger tip is higher that it's joint (pointing upwards)
+def finger_is_up(hand_landmarks, finger_tip, finger_pip):
+    return hand_landmarks.landmark[finger_tip].y < hand_landmarks.landmark[finger_pip].y
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -23,14 +29,21 @@ while cap.isOpened():
 
             # Landmarks:
             # 8 = tip of index finger
+            # 12 = tip fo middle finger
             # 5 = base of index finger (MCP joint)
-            tip_y = hand_landmarks.landmark[8].y
-            base_y = hand_landmarks.landmark[5].y
+            indexTip_y = hand_landmarks.landmark[8].y
+            middleTip_y = hand_landmarks.landmark[12].y
 
-            if tip_y < base_y:
+            indexBase_y = hand_landmarks.landmark[5].y
+
+            # Scrolls up if index fingertip is higher than it's knuckle and midlle fingers 
+            # tip (it checks that the user doesn't have whole hand open)
+            if indexTip_y < indexBase_y and indexTip_y < middleTip_y:
                 cv2.putText(frame, "SCROLL UP", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
                 pyautogui.scroll(30)
-            else:
+   
+            # Scrolls down if index fingertip is lower than it's knuckle and the middle fingers tip. 
+            if indexTip_y > indexBase_y and indexTip_y > middleTip_y:
                 cv2.putText(frame, "SCROLL DOWN", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
                 pyautogui.scroll(-30)
 
